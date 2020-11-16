42c6b5fb350212b7a2bb5dbc90835c08e97f157a
Pranay Kumar Verma <50574225+pranaykmr@users.noreply.github.com>
2020-11-16 12:29:19 -0500

    Update run.py

--- run.py
+++ run.py
@@ -34,6 +34,29 @@ def home():
 def sessionList():
     return render_template('sessionList.html')
 
+@app.route('/indexAdmin.html')
+def indexAdmin():
+    return render_template('indexAdmin.html')
+
+@app.route('/addStudent.html')
+def addStudent():
+    return render_template('addStudent.html')
+
+@app.route('/createSession.html')
+def createSession():
+    return render_template('createSession.html')
+
+@app.route('/dummy.html')
+def dummy():
+    return render_template('dummy.html')
+
+@app.route('/sessionListAdmin.html')
+def sessionListAdmin():
+    return render_template('sessionListAdmin.html')
+
+@app.route('/studentList.html')
+def studentList():
+    return render_template('studentList.html')
 
 @app.route('/examPage.html')
 def examPage():
